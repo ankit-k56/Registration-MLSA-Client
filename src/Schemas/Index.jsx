90a00daef29b515fee3538fe057382eb5a0f15fc
Ankit <ankitkumar19041@gmail.com>
2023-09-25 15:22:41 +0530

    fixing checkbox not mandatory issue

--- src/Schemas/Index.jsx
+++ src/Schemas/Index.jsx
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(30).required("Name Required"),
   rollNumber: Yup.string().min(6).max(9).required("Roll Number Required"),
-  // age: Yup.number().required('Age is required').min(16).max(25),
+  age: Yup.number().required('Age is required').min(16).max(25),
   currentYear: Yup.string().min(1).max(1).required("Current Year Required"),
   branch: Yup.string().min(1).max(25).required("Branch Required").matches("^(?=.*[A-Za-z])"),
   kiitEmailId: Yup.string().email().required("Kiit Email Required").matches("^[A-Z0-9._%+-]+@kiit\.ac\.in"),
@@ -13,5 +13,5 @@ export const signUpSchema = Yup.object({
   linkedin: Yup.string().min(2).max(100),
   github: Yup.string().min(2).max(100),
   expectation: Yup.string(),
-  checkbox: Yup.boolean().required("Please consent to continue"),
+  checkbox: Yup.boolean().test("is-true", "Please consent to continue", (value) => value === true),
 });
